1fdc4bf7509ba5f171cef413962ce8124c9250de
Fabian Pasin <fabian.pasin@gmail.com>
2022-07-02 21:16:29 +0200

    connect db via http req, start filter tutors

--- src/app/bookingview/main-booking-view/main-booking-view.component.ts
+++ src/app/bookingview/main-booking-view/main-booking-view.component.ts
@@ -1,66 +1,67 @@
+import { DatabaseService } from './../../../services/database.service';
 import { Component, OnInit} from '@angular/core';
+import { BehaviorSubject, switchMap, filter, Observable, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-main-booking-view',
   templateUrl: './main-booking-view.component.html',
   styleUrls: ['./main-booking-view.component.scss']
 })
+
 export class MainBookingViewComponent implements OnInit {
   public questions: Object[] = [
-    {index: 0, text: "Which language should your teacher speak?"},
-    {index: 1, text: "What subject are you looking for?"},
-    {index: 2, text: "And what's the purpose of the lesson?"}
+    {key: 'languageSpoken', text: "Which language should your teacher speak?"},
+    {key: 'subject', text: "What subject are you looking for?"},
+    {key: 'type', text: "And what's the purpose of the lesson?"}
   ]
 
-  public tutors: Object[] = [
-    {
-      name: "Josh",
-      lastname: "Miller",
-      subjects: [
-        {
-          name: "English",
-          types: [
-            "Conversation",
-            "Exam Preparation",
-            "Grammar"
-          ],
-          levels: [
-            "Primary school",
-            "Highschool",
-            "University"
-          ],
-        },
-        {
-          name: "German",
-          types: [
-            "Conversation",
-            "Exam Preparation",
-            "Grammar"
-          ],
-          levels: [
-            "Primary school",
-            "Highschool",
-            "University"
-          ],
-        }
-      ],
-      description: "Lorem ispum dolor sit amet, lorem ispum dolor sit amet. Lorem ispum dolor sit amet..."
-    }
-  ]
+  public tutors: string[] = [];
   public currStep = 0;
   public isTutorList = false;
+  public queryData: string[] = [];
+  public filteredTutors: any[] = [];
+  private _dbService;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(dbService: DatabaseService) { 
+    this._dbService = dbService;
   }
 
-  public setStep() {
+  ngOnInit(): void {
+    this._dbService.getTutors().pipe(
+      map(tutors => {
+        tutors.forEach((tutor: any) => this.tutors.push(tutor))
+      })
+    )
+    .subscribe()
+  }
+
+  public setStep($value: any, $key: string) {
+    this.queryData.push($value)
     this.currStep += 1;
-    console.log(this.currStep);
     
     if(this.currStep >= this.questions.length) {
+      this.filterTutors(this.tutors);
      this.isTutorList = !this.isTutorList;
     }   
+  }
+
+  public filterTutors(tutors: any) {
+    this.filteredTutors = tutors.filter((tutor: any) => {
+      return tutor.languagesSpoken.includes(this.queryData[0]);
+    }   
+    )
+    .filter((tutor: any) => {
+      const subject = this.queryData[1];
+      return tutor.subjects.some((subj: any) => subj.name === subject);
+     })
+    .filter((tutor: any) => {
+      const subject = this.queryData[1];
+      const subjTypes = tutor.subjects.find((subj: any) => subj.name === subject).types;
+      return (
+        subjTypes
+          .map((type: string) => type.toLowerCase())
+          .includes(this.queryData[2].toLowerCase())
+        )
+    })
   }
 }
