c34ccca02ddafed5db9a534f106d3d13fe1a45af
Fabian Pasin <fabian.pasin@gmail.com>
2022-07-12 10:54:33 +0200

    update enrvironment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,5 +8,6 @@ export const environment = {
     messagingSenderId: "1094650223829",
     appId: "1:1094650223829:web:8f17f915d7058127a9965c",
     measurementId: "G-01B4ZKM3J0"  
-  }
+  },
+  socketUrl: 'https://et-server.herokuapp.com/'
 };
